4da4446a2598aca1d23557e0b5238f90df59c442
omerl01 <01omerlevi@gmail.com>
2026-03-29 11:59:33 +0300

    loops pratice added

--- loops/for_loops.py
+++ loops/for_loops.py
@@ -1,16 +1,134 @@
-friends = ["rolf", "jen", "bob", "anne"]
-for friend in friends:
-    print(f"{friend} is my friend")
+# friends = ["rolf", "jen", "bob", "anne"]
+# for friend in friends:
+#     print(f"{friend} is my friend")
     
-#range()
-for number in range(4):
-    print(f"{number} is my friend")
+# #range()
+# for number in range(4):
+#     print(f"{number} is my friend")
     
-grades = [80, 75, 90, 100, 85]
+# grades = [80, 75, 90, 100, 85]
 
-total = 0 
-amout = len(grades)
-# for grade in grades:
-#     total += grade
-total = sum(grades)
-print(total/amout)
+# total = 0 
+# amout = len(grades)
+# # for grade in grades:
+# #     total += grade
+# total = sum(grades)
+# print(total/amout)
+
+# task 1
+# numbers = [1,2,3,4,5]
+# for number in numbers:
+#     print(number)
+
+# task 2
+# numbers = [5,4,3,2,1]
+# for num in numbers:
+#     print(num)
+
+# task 3
+# numbers = [2,4,6,8,10]
+# for num in numbers:
+#     print(num)
+
+# # task 4
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+# for num in numbers:
+#     print(num)
+
+# task 5
+# for i in range(3,20, 3):
+#     if i % 3 == 0:
+#         print(i)
+
+# #task 6
+# for num in range(5):
+#     print("hello")
+
+# task 7
+# for num in range(1,10):
+#     print(num)
+
+# task 8 
+# for num in range(2,21,2):
+#     print(num)
+
+# task 9 
+# for num in range(0,11):
+#     print(num)
+
+# task 10
+# num_sum = 0
+# for num in range(1,101):
+#     num_sum += num
+# print(num_sum)
+
+# task 11
+# numes = ["jermey", "anna", "bob"]
+# for name in numes:
+#     print(name)
+
+# task 12
+# names = ["jermey", "anna", "bob"]
+# items = 0
+# for name in names:
+#     items += 1
+# print(items)
+
+
+# task 13
+# numbers = [5,7,6,8,10,12]
+# num_sum = 0
+# for num in numbers:
+#     num_sum += num
+# print(num_sum)
+
+# task 14
+# numbers = [5,7,6,8,15,10,12]
+# biggest_num = 0
+# for number in numbers:
+#     if number > biggest_num:
+#         biggest_num = number
+# print(biggest_num)
+
+
+# task 15
+
+# names = ["james", "natalie", "Anna", "Abraham"]
+# for name in names:
+#     first_letter = name[0]
+#     if first_letter == "A":
+#         print(name)
+
+
+# task 16
+# numbers = [4,7,6,5,10,22,23]
+# for number in numbers:
+#     if number % 2 == 0:
+#         print(number) 
+
+# task 17
+# numbers = [4,7,6,5,10,22,23]
+# bigger = 0
+# for number in numbers:
+#     if number > 10:
+#         bigger +=1
+# print(bigger)
+
+# task 18
+# numbers = [4,7,6,5,10,12,22,23]
+# for num in numbers:
+#     if num % 2 == 0 and num % 3 == 0:
+#         print(num)
+
+# task 19
+# numbers = [4,7,6,5,10,12,22,23]
+# for num in numbers:
+#     print(num **2)
+
+# task 20
+numbers = [4,7,6,5,10,12,22,23]
+even_numbers = []
+for num in numbers:
+    if num % 2 == 0:
+        even_numbers.append(num)
+print(even_numbers)
--- loops/while_loops.py
+++ loops/while_loops.py
@@ -1,15 +1,168 @@
-# while loops basics
-# user_input = input("Would you like to paly? (n/y)")
+# # while loops basics
+# # user_input = input("Would you like to paly? (n/y)")
 
-# user_input = "y"
-# while user_input != "n":
-#     print("Game is Running...")
-    # user_input = input("Would you like to play again (n/y)")
+# # user_input = "y"
+# # while user_input != "n":
+# #     print("Game is Running...")
+#     # user_input = input("Would you like to play again (n/y)")
     
     
+# while True:
+#     user_input = input("Would you like to paly? (n/y)")
+    
+#     if user_input == "n":
+#         break
+#     print("Game is runing")
+
+# task 1
+# num = 1
+# while num < 6:
+#     print(num)
+#     num +=1
+    
+# task 2
+# num = 5
+# while num > 0:
+#     print(num)
+#     num -= 1
+
+# task 3
+# num = 2
+# while num <= 10:
+#     print(num)
+#     num +=2
+
+# task 4
+# num_sum = 0
+# number = 1
+# while number < 6:
+#     num_sum += number
+#     number += 1
+# print(num_sum)
+
+# task 5
+# num = 3
+# while num < 10:
+#     print(num)
+#     num = num * 3
+    
+# task 6
+# user_num = input("enter number: ")
+# prints = 1
+# while prints < 6:
+#     print(user_num)
+#     prints += 1
+
+# task 7
+# user_num = input("enter number: ")
+# num = 1
+# while num != int(user_num) + 1:
+#     print(num)
+#     num +=1
+# task 8 
+# enter_0 = False
+
+# while not enter_0:
+#     user_input = input("enter a number: ")
+#     if  user_input == "0":
+#         enter_0 = True
+
+# task 9 
+# correct_pass = False
+# while correct_pass == False:
+#     user_pass = input("enter a password ")
+#     if user_pass == "1234":
+#         correct_pass = True
+
+# task 10
+# proceed = True
+# while proceed:
+#     user_input = input("enter y to continue and n to stop ")
+#     if user_input == "n":
+#         proceed = False
+
+# task 11
+# num = 1
+# while num < 10:
+#     if num % 2 == 0:
+#         print(num)
+#     num += 1
+
+# task 12
+# even_num = []
+# num = 1
+# while num < 20:
+#     if num % 2 == 0:
+#         even_num.append(num)
+#     num += 1
+# print(len(even_num)) 
+
+# task 13
+# num_sum = 0 
+# num = 1
+# while num <=10:
+#     if num % 2 != 0:
+#         num_sum += num
+#     num +=1
+# print(num_sum)
+
+# task 14
+# game_on = True
+# while game_on:
+#     user = input("enter a number: ")
+#     if user == "q":
+#         game_on = False
+#     elif int(user) % 2 == 0:
+#         print("even")
+#     else:
+#         print("odd")
+    
+
+# task 15
+# entries = 1
+# numbers = []
+# while entries < 6:
+#     number = int(input("enter a number "))
+#     numbers.append(number)
+#     entries += 1
+# print(max(numbers))
+
+# task 16
+# while True:
+#     user= input("enter exit: ")
+#     if user == "exit":
+#         break
+
+# task 17
+# num = 1
+# while num < 11:
+#     print(num)
+#     num += 1
+#     if num == 7:
+#         break
+
+# task 18
+# num_sum = 0
+# while True:
+#     user = int(input("enet a number: "))
+#     if user < 0:
+#         break
+#     num_sum += user
+#     print(num_sum)
+
+# task 19
+# num = 4
+# game_on = True
+# while game_on:
+#     user_guess = int(input("enter a guess: "))
+#     if user_guess == num:
+#         game_on = False
+
+# task 20
+entries = 0
 while True:
-    user_input = input("Would you like to paly? (n/y)")
-    
-    if user_input == "n":
+    user_input = input("enter: ")
+    entries += 1
+    if user_input == "stop":
         break
-    print("Game is runing")
+print(entries)
