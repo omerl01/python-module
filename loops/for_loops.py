3ff0d306dc21cf9b19911f5e4cf500faf47bf69d
omerl01 <01omerlevi@gmail.com>
2026-03-29 10:57:03 +0300

    added sum function to for_loops.py

--- loops/for_loops.py
+++ loops/for_loops.py
@@ -5,3 +5,12 @@ for friend in friends:
 #range()
 for number in range(4):
     print(f"{number} is my friend")
+    
+grades = [80, 75, 90, 100, 85]
+
+total = 0 
+amout = len(grades)
+# for grade in grades:
+#     total += grade
+total = sum(grades)
+print(total/amout)
