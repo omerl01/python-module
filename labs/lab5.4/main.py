c0ffcee5f5b152ce6f0583a834efa6b8f2f3552f
omerl01 <01omerlevi@gmail.com>
2026-03-31 11:36:10 +0300

    added lab 5.4

---
+++ labs/lab5.4/main.py
@@ -0,0 +1,95 @@
+from pathlib import Path
+import os
+import random
+import time
+from collections import Counter, deque
+import hashlib
+import calendar
+import statistics
+import webbrowser
+import shutil
+import tempfile
+# task 1
+# p = Path("test_folder/test_file.txt")
+# print(p)
+
+# task 2
+# file = "c:/python-module/basic/.venv/Scripts/python.exe"
+# print(os.path.isfile(file))
+
+# task 3
+# file = "c:/python-module/basic/.venv/Scripts/python.exe"
+# print(f"is file: {os.path.isfile(file)}")
+# print(f"is directory: {os.path.isdir(file)}")
+
+# task 4
+# print(random.randint(1,10))
+
+# task 5
+# list = ["apples", "cherries", "tomatos"]
+# print(random.choice(list))
+
+# task 6
+# num = [1,2,3,4,5,6]
+# random.shuffle(num)
+# print(num)
+
+# # task 7
+# print("waiting")
+# time.sleep(3)
+# print("Done")
+
+
+# task 8 
+# print(time.time())
+
+# task 9 
+# list = ["a","b", "a","b","a"]
+# print(Counter(list))
+
+# task 10
+# queue = deque([1,2,3])
+# print(queue.popleft())
+
+# task 11
+# data = "hello".encode()
+# hash_data = hashlib.md5(data)
+# result = hash_data.hexdigest()
+# print(result)
+
+# task 12
+# print(calendar.month(2026, 3))
+
+# task 13
+# print(statistics.mean([1,2,3,4,5]))
+
+# task 14
+# print(statistics.median([1,2,3,4,5]))
+
+# task 15
+# webbrowser.open("https://www.w3schools.com/python/ref_module_webbrowser.asp")
+# print("browser opened")
+
+# task 16
+# source  = "c:/python-module/labs/lab5.4/main.py"
+# destination = "c:/python-module/labs/lab5.4/copy_main.py"
+# dest = shutil.copy(source, destination)
+
+# task 17
+# shutil.rmtree("c:/python-module/labs/lab5.4/test_dir")
+
+# task 18
+# temp = tempfile.TemporaryFile()
+# print("temporary file created")
+# temp.close()
+
+# task 19
+# import glob
+# pattern = "*py"
+# print(glob.glob(pattern))
+
+# task 20
+# random_num = random.randint(1,100)
+# print("waiting for 1 second")
+# time.sleep(1)
+# print(random_num)
---
+++ labs/lab6.1/main.py
@@ -0,0 +1,94 @@
+class Person:
+    def __init__(self, name, age, city, job):
+        self.name = name
+        self.age = age
+        self.city = city
+        self.job = job
+        
+    def introduction(self):
+        print(f"name: {self.name}, age: {self.age}, city: {self.city}, job: {self.job}")
+        
+    
+
+# person1 = Person("omer", 24, "Hadera", "Devops")
+# person1.introduction()
+
+class Car:
+    def __init__(self, brand, model, year, color):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.color = color
+        
+    def show_car(self):
+        print(f"brand: {self.brand}, model: {self.model}, year: {self.year}, color: {self.color}")
+        
+chevi = Car("chevrollete", "spark", 2017, "black")
+# chevi.show_car()
+
+class Book():
+    def __init__(self, title, author, pages, genre):
+        self.title = title
+        self.author = author
+        self.pages= pages
+        self.genre = genre
+    
+    def show_book(self):
+        print(self.title, self.genre)
+    
+notebook = Book("meditations", "marcus aurelius", 345, "philosopy")
+
+# notebook.show_book()
+
+class Dog:
+    def __init__(self, name, age, breed, weight):
+        self.name = name
+        self.age = age
+        self.breed = breed
+        self.weight = weight
+    
+    def show_dog(self):
+        print(self.name, self.age, self.breed, self.weight)
+
+
+ceaser = Dog("caesar", 9, "chitzu", 5)
+# ceaser.show_dog()
+
+class Phone:
+    def __init__(self, brand, model, price, battery):
+        self.brand = brand
+        self.model = model
+        self.price = price
+        self.battery = battery
+    
+    def show(self):
+        print(self.price)
+iphone = Phone("ipone", 12, 2300, 80)
+# iphone.show()
+
+
+class Student:
+    def __init__(self, name , age, grade, school):
+        self.name = name
+        self.age = age
+        self.grade = grade
+        self.school = school
+    
+    def show(self):
+        print(self.name, self.grade)
+        
+omer = Student("omer", 24, 100, "iitc")
+# omer.show()
+
+class Movie:
+    def __init__(self, title, year, rating, duration):
+        self.title = title
+        self.year= year
+        self.rating = rating
+        self.duration = duration
+    
+    def show(self):
+        print(self.rating)
+        
+apes = Movie("planet ofthe apes", 2000, 5, 130)
+apes.show()
